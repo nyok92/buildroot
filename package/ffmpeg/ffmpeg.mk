baed58488cb73d10bd8c2d73597e2ddc2729ab9a
nyok92 <40342954+nyok92@users.noreply.github.com>
2026-09-14 01:29:16 +0200

    Update FFMPEG version and site handling
    Signed-off-by: nyok92 <40342954+nyok92@users.noreply.github.com>

--- package/ffmpeg/ffmpeg.mk
+++ package/ffmpeg/ffmpeg.mk
@@ -4,12 +4,14 @@
 #
 ################################################################################
 
-FFMPEG_VERSION = ($(call qstrip,$(BR2_PACKAGE_FFMPEG_VERSION)),9.0.1)
-FFMPEG_SOURCE = ffmpeg-$(FFMPEG_VERSION).tar.xz
-FFMPEG_SITE = https://ffmpeg.org/releases
-
 ifeq ($(BR2_PACKAGE_FFMPEG_CUSTOM_GIT),y)
 FFMPEG_SITE_METHOD = git
+FFMPEG_SITE = ($(call qstrip,$(BR2_PACKAGE_FFMPEG_CUSTOM_REPO_URL)))
+FFMPEG_VERSION = ($(call qstrip,$(BR2_PACKAGE_FFMPEG_VERSION)))
+else
+FFMPEG_VERSION = 9.0.1
+FFMPEG_SITE = https://ffmpeg.org/releases
+FFMPEG_SOURCE = ffmpeg-$(FFMPEG_VERSION).tar.xz
 endif
 
 FFMPEG_INSTALL_STAGING = YES
